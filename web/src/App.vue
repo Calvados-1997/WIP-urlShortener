e439073951764d943b367d50b865f8dce5273aef
Jo Lily <lily@example.com>
2026-07-07 00:44:48 +0900

    ホーム画面のスタイル定義を追加

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,7 +3,9 @@ import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <HomeView />
+  <div class="w-full h-full min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900">
+    <HomeView />
+  </div>
 </template>
 
 <style scoped></style>
