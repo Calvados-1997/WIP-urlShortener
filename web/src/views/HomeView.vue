fc143af56023e5e888d77565d0f394e7d038ada8
Jo Lily <lily@example.com>
2026-07-07 00:45:40 +0900

    ホーム画面のレイアウト整形、その他処理実装(TODO込み)

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputText from '@/components/InputText.vue'
+import SimpleButton from '@/components/SimpleButton.vue'
+import { ref } from 'vue'
+
+const inputURL = ref<string>('')
+const createURL = (): void => {
+  // TODO: URL作成処理
+  console.log('URL作成ボタンが押下されました。')
+}
+</script>
 
 <template>
-  <div>ホーム画面</div>
+  <template class="flex items-start justify-center p-20">
+    <div
+      class="w-110 flex flex-col gap-7 pt-10 pb-20 px-6 bg-slate-800 rounded-3xl border-slate-300 shadow-2xl"
+    >
+      <label class="text-sm font-medium text-slate-300" for="url">URLを入力</label>
+      <InputText v-model="inputURL" id="url" :placeholder="'ようこそ'" />
+      <SimpleButton @click="createURL">URLを作成</SimpleButton>
+    </div>
+  </template>
 </template>
 
 <style scoped></style>
