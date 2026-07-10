3b3e7e83a8de44fbd25684a50796abd92f8a49a6
Jo Lily <lily@example.com>
2026-07-10 22:18:30 +0900

    Layoutコンポーネントを使用した実装に改修

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import MainLayout from './layouts/mainLayout.vue'
 import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <div class="w-full h-full min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900">
-    <HomeView />
-  </div>
+  <MainLayout>
+    <template #header>TODO: This is header area</template>
+    <template #content>
+      <HomeView />
+    </template>
+    <template #footer>TODO: This is footer</template>
+  </MainLayout>
 </template>
 
 <style scoped></style>
