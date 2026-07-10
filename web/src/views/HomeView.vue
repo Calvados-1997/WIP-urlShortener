33344ea1b14ae84477e54723b99c8212a808f858
Jo Lily <lily@example.com>
2026-07-10 22:17:57 +0900

    不要なスタイル、タグを削除

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -11,15 +11,13 @@ const createURL = (): void => {
 </script>
 
 <template>
-  <template class="flex items-start justify-center p-20">
-    <div
-      class="w-110 flex flex-col gap-7 pt-10 pb-20 px-6 bg-slate-800 rounded-3xl border-slate-300 shadow-2xl"
-    >
-      <label class="text-sm font-medium text-slate-300" for="url">URLを入力</label>
-      <InputText v-model="inputURL" id="url" :placeholder="'ようこそ'" />
-      <SimpleButton @click="createURL">URLを作成</SimpleButton>
-    </div>
-  </template>
+  <div
+    class="w-110 flex flex-col gap-7 pt-10 pb-20 px-6 bg-slate-800 rounded-3xl border-slate-300 shadow-2xl"
+  >
+    <label class="text-sm font-medium text-slate-300" for="url">URLを入力</label>
+    <InputText v-model="inputURL" id="url" :placeholder="'ようこそ'" />
+    <SimpleButton @click="createURL">URLを作成</SimpleButton>
+  </div>
 </template>
 
 <style scoped></style>
