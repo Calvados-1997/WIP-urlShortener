ac0653ffa6f2b34537691337770e9dc4fae15eb0
Jo Lily <lily@example.com>
2026-07-11 00:21:53 +0900

    WIP: URL短縮化処理の呼び出しメソッドを定義

--- web/src/stores/urlShortener.ts
+++ web/src/stores/urlShortener.ts
@@ -1,3 +1,23 @@
+import { http } from '@/api/client'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useURLShortenerStore = defineStore('URLShortener', () => {})
+// 仮
+type ShortenURLRequest = {
+  url: string
+}
+type ShortenURLResponse = {
+  url: string
+}
+
+export const useURLShortenStore = defineStore('URLShortener', () => {
+  const shortenedURL = ref<string>()
+
+  const shortenURL = async (targetURL: string) => {
+    const req: ShortenURLRequest = { url: targetURL }
+    const res = await http.post<ShortenURLResponse>('/urls', req)
+    shortenedURL.value = res.url
+  }
+
+  return { shortenedURL, shortenURL }
+})
